818ac0f25ba41d805d9cc5496b063e591fd07ed8
tgasparre <gasparre@chapman.edu>
2026-03-19 10:12:04 -0700

    rebalanced knockback and hitstun duration

--- Assets/Scripts/Combat/CombatParameters.cs
+++ Assets/Scripts/Combat/CombatParameters.cs
@@ -6,10 +6,10 @@ public static class CombatParameters
     public const int ultimateAttackDamage = 40;
 
     public const float knockbackDuration = 0.2f;
-    public const float knockbackForce = 20f; //was 5.5f
+    public const float knockbackForce = 7f; //was 5.5f
     public static readonly AnimationCurve knockbackCurve = AnimationCurve.EaseInOut(0, 1, 1, 0);
     
     public const float ultimateKnockbackForce = 5f;
-    public const float hitStunDuration = 0.2f;
+    public const float hitStunDuration = 0.5f;
 }
 
